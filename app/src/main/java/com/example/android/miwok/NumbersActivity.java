dfa89f2b2f9b09bd9592cee337e41eea7d9e06ba
M Ola <ola466@gmail.com>
2018-01-15 16:07:53 -0500

    add for loops

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -1,19 +1,25 @@
 package com.example.android.miwok;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
-
+Context context;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
         ArrayList<String> words=new ArrayList<String>();
 
-words.add("one");
+       // LinearLayout rootView= (LinearLayout) findViewById(R.id.rootView);
+
+
+                words.add("one");
 words.add("two");
 words.add("three");
 words.add("four");
@@ -24,8 +30,18 @@ words.add("eight");
 words.add("nine");
 words.add("ten");
 
+        LinearLayout rootView= (LinearLayout) findViewById(R.id.rootView);
 
 
+for (int index=0; index<words.size();index++){
+
+    TextView wordView=new TextView(context);
+    wordView.setText(words.get(index));
+
+    // Add this TextView as another child to the root view of this layout
+    rootView.addView(wordView);
+
+}
 
     }
 }
