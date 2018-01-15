424ca7fcee3828f72791344d9542dc2b32311642
M Ola <ola466@gmail.com>
2018-01-14 22:39:44 -0500

    Initial commit

---
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -0,0 +1,31 @@
+package com.example.android.miwok;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+public class NumbersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_numbers);
+        ArrayList<String> words=new ArrayList<String>();
+
+words.add("one");
+words.add("two");
+words.add("three");
+words.add("four");
+words.add("five");
+words.add("six");
+words.add("even");
+words.add("eight");
+words.add("nine");
+words.add("ten");
+
+
+
+
+    }
+}
